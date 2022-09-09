5492605dd6b09d7f2fb1c148568a8c33fbfc54d8
kenta1114 <61451524+kenta1114@users.noreply.github.com>
2022-09-09 13:54:09 +0900

    Add files via upload

---
+++ todo_project/urls.py
@@ -0,0 +1,9 @@
+"""todo_project URL Configuration"""
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include ("todo_app.urls"))
+]
